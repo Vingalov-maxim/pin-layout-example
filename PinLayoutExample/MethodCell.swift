274ad65b4e218ef5c765d6a42f1a6b74324f32a0
Vingalov-maxim <mastergmv@gmail.com>
2017-10-02 22:47:52 +0700

    added some elements to Method cell

--- PinLayoutExample/MethodCell.swift
+++ PinLayoutExample/MethodCell.swift
@@ -7,13 +7,41 @@
 //
 
 import UIKit
+import PinLayout
 
 class MethodCell: UITableViewCell {
     static let reuseIdentifier = "MethodCell"
     
+    fileprivate let profileImageView = UIImageView(image: UIImage(named: "method"))
     fileprivate let iconImageView = UIImageView(image: UIImage(named: "method"))
     fileprivate let nameLabel = UILabel()
+    fileprivate let createdAtLabel = UILabel()
     fileprivate let descriptionLabel = UILabel()
+    fileprivate let topSeparator = UIView()
+    fileprivate let bottomSeparator = UIView()
+    fileprivate let bottomWrapper = UIView()
+
+    lazy var likeButton: UIButton = {
+        let button = self.createButton(text: "", imageName: "like_btn", titleOffset: 6.0, imageOffset: 0.0)
+        button.contentHorizontalAlignment = .left
+        
+        return button
+    }()
+
+    lazy var commentButton: UIButton = {
+        let button = self.createButton(text: "", imageName: "comment_btn", titleOffset: 6.0, imageOffset: 0.0)
+        button.contentHorizontalAlignment = .center
+        
+        return button
+    }()
+    
+    lazy var repostButton: UIButton = {
+        let button = self.createButton(text: "", imageName: "repost_btn", titleOffset: 0.0, imageOffset: 6.0)
+        button.contentHorizontalAlignment = .right
+        
+        return button
+    }()
+
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -22,7 +50,8 @@ class MethodCell: UITableViewCell {
         separatorInset = .zero
         
         contentView.addSubview(iconImageView)
-        
+        contentView.addSubview(profileImageView)
+
         nameLabel.font = UIFont.boldSystemFont(ofSize: 14)
         nameLabel.lineBreakMode = .byTruncatingTail
         contentView.addSubview(nameLabel)
@@ -30,6 +59,23 @@ class MethodCell: UITableViewCell {
         descriptionLabel.font = UIFont.systemFont(ofSize: 14)
         descriptionLabel.numberOfLines = 0
         contentView.addSubview(descriptionLabel)
+        
+        createdAtLabel.font = UIFont.systemFont(ofSize: 12)
+        createdAtLabel.numberOfLines = 0
+        contentView.addSubview(createdAtLabel)
+        
+        topSeparator.backgroundColor = .lightGray
+        bottomSeparator.backgroundColor = .lightGray
+        
+        contentView.addSubview(topSeparator)
+        contentView.addSubview(bottomSeparator)
+        
+        contentView.addSubview(bottomWrapper)
+        
+        bottomWrapper.addSubview(likeButton)
+        bottomWrapper.addSubview(commentButton)
+        bottomWrapper.addSubview(repostButton)
+
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -42,6 +88,10 @@ class MethodCell: UITableViewCell {
         
         descriptionLabel.text = method.description
         descriptionLabel.sizeToFit()
+        
+        createdAtLabel.text = method.name
+        descriptionLabel.sizeToFit()
+
     }
     
     override func layoutSubviews() {
@@ -51,15 +101,24 @@ class MethodCell: UITableViewCell {
     
     @discardableResult
     fileprivate func layout() -> CGSize {
-        let margin: CGFloat = 10
+        let margin: CGFloat = 16
         
         // 1) Layout controls
-        iconImageView.pin.top().left().size(30).margin(margin)
-        nameLabel.pin.right(of: iconImageView, aligned: .center).right().marginHorizontal(margin).fitSize()
-        descriptionLabel.pin.below(of: [iconImageView, nameLabel]).left().right().margin(margin).fitSize()
+        profileImageView.pin.topLeft().size(40).margin(margin)
+        nameLabel.pin.right(of: profileImageView, aligned: .top).right().marginHorizontal(16).fitSize()
+        createdAtLabel.pin.right(of: profileImageView, aligned: .bottom).right().marginHorizontal(16).fitSize()
+        topSeparator.pin.below(of: profileImageView).left().right().height(1).marginTop(8)
+        iconImageView.pin.below(of: topSeparator).left().right().height(UIScreen.main.bounds.width - margin * 2).margin(margin)
+        descriptionLabel.pin.below(of: iconImageView).left().right().margin(margin).fitSize()
         
+        bottomSeparator.pin.below(of: descriptionLabel).left().right().height(1).marginTop(margin)
+        bottomWrapper.pin.below(of: bottomSeparator).left().right().height(40).marginHorizontal(16)
+        likeButton.pin.topLeft().bottom().width(30%)
+        commentButton.pin.right(of: likeButton, aligned: .top).bottom().width(40%)
+        repostButton.pin.right(of: commentButton, aligned: .top).bottom().width(30%)
+
         // 2) Returns a size that contains all controls
-        return CGSize(width: contentView.frame.width, height: descriptionLabel.frame.maxY + margin)
+        return CGSize(width: contentView.frame.width, height: bottomWrapper.frame.maxY + margin)
     }
     
     override func sizeThatFits(_ size: CGSize) -> CGSize {
@@ -69,5 +128,26 @@ class MethodCell: UITableViewCell {
         // 2) Layout the contentView's controls
         return layout()
     }
+    
+    func createButton(text: String, imageName: String, titleOffset: CGFloat, imageOffset: CGFloat) -> UIButton {
+        
+        let button = UIButton(type: .system)
+        
+        let font = UIFont.systemFont(ofSize: 15)
+        
+        let attributedString = NSAttributedString(string: text, attributes: [NSAttributedStringKey.font: font, NSAttributedStringKey.foregroundColor: UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1.0)])
+        
+        button.backgroundColor = .white
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.setAttributedTitle(attributedString, for: .normal)
+        
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, titleOffset, 0, 0)
+        button.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, imageOffset)
+        
+        return button
+    }
+
+    
+    
 }
 
